c2fd993038847dc2f2beb7cd93b23ae06d041e7e
Ali Abbasov <ali3abbasov@gmail.com>
2023-09-18 09:15:42 +0400

    add: Bill to input fields

--- app/components/InvoiceCard.tsx
+++ app/components/InvoiceCard.tsx
@@ -4,22 +4,30 @@
 import { useForm } from "react-hook-form";
 
 // Zod imports
-import * as z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 // Form schemas
 import { InvoiceSchema } from "@/lib/schemas";
 
 // Shadcn components
 import {
-	Card,
+    Card,
     CardContent,
     CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
@@ -29,62 +37,169 @@ import { Label } from "@/components/ui/label";
 
 const InvoiceCard = () => {
     const form = useForm<z.infer<typeof InvoiceSchema>>({
-		resolver: zodResolver(InvoiceSchema),
-		defaultValues: {
-			sender: {
-				senderName: "",
-				senderAddress: "",
+        resolver: zodResolver(InvoiceSchema),
+        defaultValues: {
+            sender: {
+                name: "",
+                address: "",
+				zipCode: "",
+				city: "",
+				country: "",
+				email: "",
+				phone: "",
+				vatNumber: "",
+            },
+			receiver: {
+				name: "",
+				address: "",
+				zipCode: "",
+				city: "",
+				country: "",
+				email: "",
+				phone: "",
+				vatNumber: "",
 			}
-		},
-	});
+        },
+    });
 
-	const onSubmit = (values: z.infer<typeof InvoiceSchema>) => {
-		console.log("VALUE")
-		console.log(values)
-	}
+    const onSubmit = (values: z.infer<typeof InvoiceSchema>) => {
+        console.log("VALUE");
+        console.log(values);
+    };
 
     return (
-        <div className="container pt-10">
+        <div className="max-w-5xl pt-10">
             <Card>
                 <CardHeader>
                     <CardTitle>INVOICE</CardTitle>
                     <CardDescription>Generate invoice</CardDescription>
                 </CardHeader>
                 <CardContent>
-					<Form {...form}>
-						<form 
-							onSubmit={form.handleSubmit(onSubmit)} 
-							className="space-y-8"
-						>
-							<div className="flex justify-around gap-2">
-								<div className="flex flex-col gap-2">
-									<Label htmlFor="billFrom" className="text-xl font-semibold">Bill From:</Label>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="space-y-8"
+                        >
+                            <div className="flex flex-wrap justify-center gap-40">
+                                <div className="flex flex-col gap-2">
+                                    <Label
+                                        htmlFor="billFrom"
+                                        className="text-xl font-semibold"
+                                    >
+                                        Bill From:
+                                    </Label>
 
-									<InputFormField 
-										control={form.control}
-										name="sender.senderName"
-										label="Company"
-										placeholder="Your company"
-									/>
-									<InputFormField 
-										control={form.control}
-										name="sender.address"
-										label="Address"
-										placeholder="Your address"
-									/>
+                                    <InputFormField
+                                        control={form.control}
+                                        name="sender.name"
+                                        label="Company"
+                                        placeholder="Your company"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="sender.address"
+                                        label="Address"
+                                        placeholder="Your address"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="sender.zipCode"
+                                        label="Zip"
+                                        placeholder="Your zip code"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="sender.city"
+                                        label="City"
+                                        placeholder="Your city"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="sender.country"
+                                        label="Country"
+                                        placeholder="Your country"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="sender.email"
+                                        label="Email"
+                                        placeholder="Your email"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="sender.phone"
+                                        label="Phone"
+                                        placeholder="Your phone number"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="sender.vatNumber"
+                                        label="VAT Number"
+                                        placeholder="Your VAT Number"
+                                    />
+                                </div>
 
-								</div>
+                                <div className="flex flex-col gap-2">
+                                    <Label
+                                        htmlFor="billTo"
+                                        className="text-xl font-semibold"
+                                    >
+                                        Bill To:
+                                    </Label>
 
-								<div className="flex flex-col gap-2">
-
-									<div>
-										aaa
-									</div>
-								</div>
-							</div>
-							<Button type="submit">Submit</Button>
-						</form>
-					</Form>
+                                    <InputFormField
+                                        control={form.control}
+                                        name="receiver.name"
+                                        label="Company"
+                                        placeholder="Receiver company"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="receiver.address"
+                                        label="Address"
+                                        placeholder="Receiver address"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="receiver.zipCode"
+                                        label="Zip"
+                                        placeholder="Receiver zip code"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="receiver.city"
+                                        label="City"
+                                        placeholder="Receiver city"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="receiver.country"
+                                        label="Country"
+                                        placeholder="Receiver country"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="receiver.email"
+                                        label="Email"
+                                        placeholder="Receiver email"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="receiver.phone"
+                                        label="Phone"
+                                        placeholder="Receiver phone number"
+                                    />
+                                    <InputFormField
+                                        control={form.control}
+                                        name="receiver.vatNumber"
+                                        label="VAT Number"
+                                        placeholder="Receiver VAT Number"
+                                    />
+                                </div>
+                            </div>
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
                 </CardContent>
                 <CardFooter>
                     <p>Card Footer</p>
