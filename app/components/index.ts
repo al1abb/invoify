419b736cbf27e93c5f8d92e4f8c543a0bfd6321d
Ali Abbasov <ali3abbasov@gmail.com>
2023-09-18 22:44:53 +0400

    refactor: Nicely import new components

--- app/components/index.ts
+++ app/components/index.ts
@@ -3,4 +3,7 @@ import InvoiceCard from "./InvoiceCard";
 // Form fields
 import InputFormField from "./InputFormField";
 
-export { InvoiceCard, InputFormField }
+import Items from "./Items";
+import SingleItem from "./SingleItem";
+
+export { InvoiceCard, InputFormField, Items, SingleItem }
