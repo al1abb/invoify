dac635791cd95fa7ebd6c8be8c7170be97c8d25f
Ali Abbasov <ali3abbasov@gmail.com>
2023-09-20 16:36:00 +0400

    add: Initial pdf endpoint to generate pdf of invoice

---
+++ app/api/pdf/route.ts
@@ -0,0 +1,52 @@
+import puppeteer, { Page } from "puppeteer";
+import fs from "fs";
+import path from "path";
+import handlebars from "handlebars";
+
+export async function POST(req: Request, res: Response) {
+    try {
+        const body = await req.json();
+
+		// Assuming this API file is in the 'pages/api' directory
+		const templatePath = path.join(process.cwd(), "templates", "invoice-template-1.handlebars");
+
+		// Read the HTML template from a file
+        const htmlTemplate = fs.readFileSync(templatePath, "utf8");
+		
+		// Compile the Handlebars template
+		const template = handlebars.compile(htmlTemplate);
+		
+		// Render the HTML with dynamic data
+        const htmlContent = template(body);
+		
+        // Create a Puppeteer browser instance
+        const browser = await puppeteer.launch();
+        const page: Page = await browser.newPage();
+
+        // Set the HTML content of the page
+        await page.setContent(htmlContent);
+
+        // Generate the PDF
+        const pdf: Buffer = await page.pdf({
+            format: "Letter", // You can change the page format here
+            printBackground: true,
+        });
+
+        // Close the Puppeteer browser
+        await browser.close();
+
+        const response = new Response(pdf, {
+            headers: {
+                "Content-Type": "application/pdf",
+                "Content-Disposition": "attachment; filename=invoice.pdf",
+            },
+            status: 200,
+        });
+        return response;
+    } catch (error) {
+        console.error(error);
+
+        // Return an error response
+        return new Response(`Error generating PDF: \n ${error}`, { status: 500 });
+    }
+}
