ee2feadf2c0e56f585b1fccafc1f626d778aa8e3
Ali Abbasov <ali3abbasov@gmail.com>
2023-09-21 19:27:35 +0400

    add: Use currencies custom hook

---
+++ app/hooks/useCurrencies.ts
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+// Variables
+import { CURRENCIES } from '@/lib/variables';
+
+const useCurrencies = () => {
+    const [currencies, setCurrencies] = useState<any>([]);
+    const [currenciesLoading, setCurrenciesLoading] = useState<boolean>(false);
+
+    /**
+     * Fetches all the currencies asynchronously.
+     *
+     * @return {Promise<void>} Promise that resolves when the currencies are fetched.
+     */
+    const fetchCurrencies = async () => {
+        setCurrenciesLoading(true)
+
+        try {
+            const response = await fetch(`${CURRENCIES}`);
+            const data = await response.json()
+
+            const currencyOptions = Object.keys(data).map((currencyCode) => {
+                const currencyName = data[currencyCode];
+                return { code: currencyCode, name: currencyName };
+            });
+
+            setCurrencies(currencyOptions)
+        } catch(err) {
+            console.log(err)
+        } finally {
+            setCurrenciesLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchCurrencies()
+    }, [])
+
+    return { currencies, currenciesLoading, fetchCurrencies }
+}
+
+export default useCurrencies
