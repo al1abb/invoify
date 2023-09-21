aefdb1b28b5e4ffb7d03435d9081d76ff7f19980
Ali Abbasov <ali3abbasov@gmail.com>
2023-09-21 19:31:53 +0400

    refactor: Updated component import file

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,11 +1,24 @@
 import InvoiceCard from "./InvoiceCard";
 
 // Form fields
-import InputFormField from "./InputFormField";
+import InputFormField from "./form-fields/InputFormField";
+import SelectFormField from "./form-fields/SelectFormField";
+import DatePickerFormField from "./form-fields/DatePickerFormField";
 
 import Items from "./Items";
 import SingleItem from "./SingleItem";
-
 import InvoiceFooter from "./InvoiceFooter";
 
-export { InvoiceCard, InputFormField, Items, SingleItem, InvoiceFooter }
+// React-PDF
+import PdfViewer from "./PdfViewer";
+
+export {
+    InvoiceCard,
+    Items,
+    SingleItem,
+    InvoiceFooter,
+    PdfViewer,
+    InputFormField,
+    SelectFormField,
+    DatePickerFormField
+};
