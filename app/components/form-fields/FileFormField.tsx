04a8621369a4b88e3cec50c96187878b318b805d
Ali Abbasov <ali3abbasov@gmail.com>
2023-09-23 22:25:22 +0400

    add: Puppeteer config file
    In addition, add .cache folder to gitignore

--- app/components/form-fields/FileFormField.tsx
+++ app/components/form-fields/FileFormField.tsx
@@ -10,7 +10,6 @@ import {
     FormItem,
     FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
