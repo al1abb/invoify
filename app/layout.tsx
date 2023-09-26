1cd7fcf44731e502bc172d28dea4fa3fe98bdbdf
Ali Abbasov <ali3abbasov@gmail.com>
2023-09-26 22:04:11 +0400

    refactor: Update webpage font
    Change default inter font to outfit font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Outfit } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const outfit = Outfit({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Invoice App',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={outfit.className}>{children}</body>
     </html>
   )
 }
