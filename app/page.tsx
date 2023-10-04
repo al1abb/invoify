f51abdfa76ef90ecdd8b4482e59f72c98ec1048e
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-04 21:12:03 +0400

    refactor: Update main container to be responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,16 @@ import { InvoiceCard } from "@/app/components";
 
 export default function Home() {
     return (
-        <main className="container sm:container-lg py-10 sm:py-4">
+        <main className="lg:container md:container sm:container-lg py-10 sm:py-4 xs:container-lg">
+
+            {/* //TODO: Replace this with Navbar component. */}
+            <Image 
+                src={"/assets/img/invoify-logo.svg"}
+                alt="Invoify Logo"
+                width={200}
+                height={100}
+            />
+
 			<InvoiceCard />
         </main>
     );
