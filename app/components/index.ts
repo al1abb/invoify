d770edcb5ec2a6b5f3de953059802486d3ede218
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-09 21:17:56 +0400

    refactor: Update import index file

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,7 +2,15 @@
 import BaseNavbar from "./navigation/BaseNavbar";
 import BaseFooter from "./navigation/BaseFooter";
 
-import InvoiceCard from "./InvoiceCard";
+// Invoice
+import InvoiceMain from "./invoice/InvoiceMain";
+import InvoiceForm from "./invoice/components/InvoiceForm";
+import InvoiceActions from "./invoice/components/InvoiceActions";
+
+import Items from "./Items";
+import SingleItem from "./SingleItem";
+import PaymentInformation from "./PaymentInformation";
+import InvoiceFooter from "./InvoiceFooter";
 
 // Form fields
 import InputFormField from "./form-fields/InputFormField";
@@ -10,10 +18,7 @@ import SelectFormField from "./form-fields/SelectFormField";
 import DatePickerFormField from "./form-fields/DatePickerFormField";
 import FileFormField from "./form-fields/FileFormField";
 
-import Items from "./Items";
-import SingleItem from "./SingleItem";
-import PaymentInformation from "./PaymentInformation";
-import InvoiceFooter from "./InvoiceFooter";
+import SavedInvoiceSelector from "./SavedInvoiceSelector";
 
 // React-PDF
 import PdfViewer from "./PdfViewer";
@@ -21,7 +26,9 @@ import PdfViewer from "./PdfViewer";
 export {
     BaseNavbar,
     BaseFooter,
-    InvoiceCard,
+    InvoiceMain,
+    InvoiceForm,
+    InvoiceActions,
     Items,
     SingleItem,
     PaymentInformation,
@@ -30,5 +37,6 @@ export {
     InputFormField,
     SelectFormField,
     DatePickerFormField,
-    FileFormField
+    FileFormField,
+    SavedInvoiceSelector,
 };
