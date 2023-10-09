c379591b7071a0130259d58a0d10d8a61ca99598
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-09 21:23:47 +0400

    refactor: Reimport invoice main to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { InvoiceCard } from "@/app/components";
+import { InvoiceMain } from "./components";
 
 export default function Home() {
     return (
         <main className="lg:container md:container sm:container-lg py-10 sm:py-4 xs:container-lg">
-            <InvoiceCard />
+            <InvoiceMain />
         </main>
     );
 }
