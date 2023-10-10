5706acb42a5676fd379efd59577430c50f573217
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-10 20:37:08 +0400

    refactor: Test executable path

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -6,5 +6,5 @@ const { join } = require("path");
 module.exports = {
     // Changes the cache location for Puppeteer.
     cacheDirectory: join(__dirname, ".cache", "puppeteer"),
-    browserRevision: "117.0.5938.88",
+    executablePath: "/vercel/path0/.cache/puppeteer/chrome/linux-117.0.5938.88",
 };
