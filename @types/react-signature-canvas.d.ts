068dd9b90df057c89cfe03286f85efee677add45
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-10 16:44:19 +0400

    refactor: Turn interface into type in signature props

--- @types/react-signature-canvas.d.ts
+++ @types/react-signature-canvas.d.ts
@@ -1,11 +1,11 @@
 declare module "react-signature-canvas" {
     import { Component, Ref } from "react";
 
-    interface SignatureCanvasProps {
+    type SignatureCanvasProps = {
         penColor?: string;
         canvasProps?: React.CanvasHTMLAttributes<HTMLCanvasElement>;
         onEnd?: React.MouseEventHandler;
-    }
+    };
 
     class SignatureCanvas extends Component<SignatureCanvasProps> {
         clear: () => void;
