fecdf7a5366021cea3f59d90ade97eff8f00e835
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-13 18:10:21 +0400

    refactor: Apply useMemo to pdfUrl variable

--- app/components/PdfViewer.tsx
+++ app/components/PdfViewer.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 // Shadcn components
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 
@@ -6,7 +8,9 @@ type PdfViewerProps = {
 };
 
 const PdfViewer = ({ pdfBlob }: PdfViewerProps) => {
-    const pdfUrl = window.URL.createObjectURL(pdfBlob);
+    const pdfUrl = useMemo(() => {
+        return window.URL.createObjectURL(pdfBlob);
+    }, [pdfBlob]);
     return (
         <>
             <p className="text-xl font-semibold my-2">PDF Preview</p>
