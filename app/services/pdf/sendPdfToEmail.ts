c3875be7664c09f8b8e0d148b867ec55ece4fc19
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-13 22:38:49 +0400

    feat: Added send pdf to email service and api route
    Initial version of send pdf feature service and api route is implemented

---
+++ app/services/pdf/sendPdfToEmail.ts
@@ -0,0 +1,34 @@
+import nodemailer, { SendMailOptions } from "nodemailer";
+import { NODEMAILER_EMAIL, NODEMAILER_PW } from "@/lib/variables";
+import SendPdfEmail from "@/app/components/templates/email/SendPdfEmail";
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: NODEMAILER_EMAIL,
+        pass: NODEMAILER_PW,
+    },
+});
+
+const sendPdfToEmail = async (
+    email: string,
+    invoicePdf: Blob
+): Promise<boolean> => {
+    try {
+        // sending email from own to own account
+        const mailOptions: SendMailOptions = {
+            from: NODEMAILER_EMAIL,
+            to: email,
+            subject: "Your invoice PDF is ready",
+            html: content,
+        };
+
+        await transporter.sendMail(mailOptions);
+        return true;
+    } catch (error) {
+        console.error("Error sending email", error);
+        return false;
+    }
+};
+
+export default sendPdfToEmail;
