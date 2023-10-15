cb10ead15b6431cc50f6e7d036486f3ac76a3b58
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-15 12:38:55 +0400

    refactor: Add send pdf success and error toasts

--- lib/toasts.ts
+++ lib/toasts.ts
@@ -1,26 +1,49 @@
 import { toast } from "@/components/ui/use-toast";
 
-function pdfGenerationSuccess() {
+const pdfGenerationSuccess = () => {
     toast({
         variant: "default",
         title: "Your invoice has been generated!",
         description: "You can preview, download, or save it",
     });
-}
-function saveInvoiceSuccess() {
+};
+const saveInvoiceSuccess = () => {
     toast({
         variant: "default",
         title: "Saved Invoice",
         description: "Your invoice details are saved now",
     });
-}
+};
 
-function modifiedInvoiceSuccess() {
+const modifiedInvoiceSuccess = () => {
     toast({
         variant: "default",
         title: "Modified Invoice",
         description: "Successfully modified your invoice",
     });
-}
+};
 
-export { pdfGenerationSuccess, saveInvoiceSuccess, modifiedInvoiceSuccess };
+// Sending to email
+const sendPdfSuccess = () => {
+    toast({
+        variant: "default",
+        title: "Email sent",
+        description: "Your invoice has been sent to the specified email",
+    });
+};
+
+const sendPdfError = () => {
+    toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Something went wrong. Try again in a moment",
+    });
+};
+
+export {
+    pdfGenerationSuccess,
+    saveInvoiceSuccess,
+    modifiedInvoiceSuccess,
+    sendPdfSuccess,
+    sendPdfError,
+};
