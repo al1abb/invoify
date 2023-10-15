e5cef8f5259882a488604acbbba426919c643a8f
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-15 20:00:13 +0400

    refactor: Add custom error handling to send pdf modal
    This error handling uses isValidEmail helper

--- app/components/modals/SendPdfToEmailModal.tsx
+++ app/components/modals/SendPdfToEmailModal.tsx
@@ -20,6 +20,9 @@ import { BaseButton } from "@/app/components";
 // Icons
 import { Mail } from "lucide-react";
 
+// Helpers
+import { isValidEmail } from "@/lib/helpers";
+
 type SendPdfToEmailModalProps = {
     sendPdfToMail: (email: string) => Promise<void>;
 };
@@ -28,50 +31,68 @@ const SendPdfToEmailModal = ({ sendPdfToMail }: SendPdfToEmailModalProps) => {
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
     const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+    const errorMessage = "Please enter a valid email address";
 
     const handleSendPdf = () => {
         setLoading(true);
 
-        sendPdfToMail(email).finally(() => {
+        if (isValidEmail(email)) {
+            sendPdfToMail(email).finally(() => {
+                setError("");
+                setLoading(false);
+                setEmail("");
+                setOpen(false);
+            });
+        } else {
             setLoading(false);
-            setEmail("");
-            setOpen(false);
-        });
+            setError(errorMessage);
+        }
     };
 
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger asChild>
-                <BaseButton tooltipLabel="Send invoice PDF to mail" size="icon">
-                    <Mail />
-                </BaseButton>
-            </DialogTrigger>
+        <form>
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogTrigger asChild>
+                    <BaseButton
+                        tooltipLabel="Send invoice PDF to mail"
+                        size="icon"
+                    >
+                        <Mail />
+                    </BaseButton>
+                </DialogTrigger>
 
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Send to email</DialogTitle>
-                    <DialogDescription>
-                        Please specify the email address for invoice delivery.
-                    </DialogDescription>
-                </DialogHeader>
-                <Label>Email</Label>
-                <Input
-                    type="email"
-                    placeholder="Enter email address"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                ></Input>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Send to email</DialogTitle>
+                        <DialogDescription>
+                            Please specify the email address for invoice
+                            delivery.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <Label>Email</Label>
+                    <Input
+                        type="email"
+                        placeholder="Enter email address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    ></Input>
 
-                <BaseButton
-                    tooltipLabel="Send invoice PDF"
-                    loading={loading}
-                    loadingText="Sending email"
-                    onClick={handleSendPdf}
-                >
-                    Send PDF
-                </BaseButton>
-            </DialogContent>
-        </Dialog>
+                    {!loading && error && (
+                        <small style={{ color: "red" }}>{error}</small>
+                    )}
+
+                    <BaseButton
+                        tooltipLabel="Send invoice PDF"
+                        loading={loading}
+                        loadingText="Sending email"
+                        onClick={handleSendPdf}
+                    >
+                        Send PDF
+                    </BaseButton>
+                </DialogContent>
+            </Dialog>
+        </form>
     );
 };
 
