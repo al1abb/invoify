0c02477bb3ecabbe9d668aa3260b554ca51e3508
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-18 23:32:00 +0400

    refactor(schema)!: Add field validators

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -6,89 +6,137 @@ import { formatNumberWithCommas } from "./helpers";
 // Variables
 import { DATE_OPTIONS } from "./variables";
 
+// TODO: Refactor some of the validators. Ex: name and zipCode or address and country have same rules
+// Field Validators
+const fieldValidators = {
+    name: z
+        .string()
+        .min(2, { message: "Must at least 2 characters" })
+        .max(50, { message: "Must be at most 50 characters" }),
+    address: z
+        .string()
+        .min(2, { message: "Must be at least 2 characters" })
+        .max(70, { message: "Must be between 2 and 70 characters" }),
+    zipCode: z
+        .string()
+        .min(2, { message: "Must be between 2 and 20 characters" })
+        .max(20, { message: "Must be between 2 and 20 characters" }),
+    city: z
+        .string()
+        .min(1, { message: "Must be between 1 and 50 characters" })
+        .max(50, { message: "Must be between 1 and 50 characters" }),
+    country: z
+        .string()
+        .min(1, { message: "Must be between 1 and 70 characters" })
+        .max(70, { message: "Must be between 1 and 70 characters" }),
+    email: z
+        .string()
+        .email({ message: "Email must be a valid email" })
+        .min(5, { message: "Must be between 5 and 30 characters" })
+        .max(30, { message: "Must be between 5 and 30 characters" }),
+    phone: z
+        .string()
+        .min(1, { message: "Must be between 1 and 50 characters" })
+        .max(50, {
+            message: "Must be between 1 and 50 characters",
+        }),
+
+    // Dates
+    date: z
+        .date()
+        .transform((date) =>
+            new Date(date).toLocaleDateString(undefined, DATE_OPTIONS)
+        ),
+
+    // Items
+    quantity: z.coerce
+        .number()
+        .min(1, { message: "Must be a number greater than 0" }),
+    unitPrice: z.coerce
+        .number()
+        .min(1, { message: "Must be a number greater than 0" }),
+
+    // Strings
+    string: z.string(),
+    stringMin1: z.string().min(1, { message: "Must be at least 1 character" }),
+    stringToNumber: z.coerce.number(),
+    stringOptional: z.string().optional(),
+    numWithCommas: z.coerce.number().transform((value) => {
+        return formatNumberWithCommas(value);
+    }),
+};
+
 const InvoiceSenderSchema = z.object({
-    name: z.string().min(2).max(50),
-    address: z.string().min(2).max(70),
-    zipCode: z.string().min(2).max(20),
-    city: z.string().min(1).max(50),
-    country: z.string().min(1).max(70),
-    email: z.string().email().min(5).max(30),
-    phone: z.string().min(1).max(50),
-    vatNumber: z.string().optional(),
+    name: fieldValidators.name,
+    address: fieldValidators.address,
+    zipCode: fieldValidators.zipCode,
+    city: fieldValidators.city,
+    country: fieldValidators.country,
+    email: fieldValidators.email,
+    phone: fieldValidators.phone,
+    vatNumber: fieldValidators.stringOptional,
 });
 
 const InvoiceReceiverSchema = z.object({
-    name: z.string().min(2).max(50),
-    address: z.string().min(2).max(70),
-    zipCode: z.string().min(2).max(20),
-    city: z.string().min(1).max(20),
-    country: z.string().min(1).max(70),
-    email: z.string().email().min(5).max(30),
-    phone: z.string().min(1).max(50),
-    vatNumber: z.string().optional(),
+    name: fieldValidators.name,
+    address: fieldValidators.address,
+    zipCode: fieldValidators.zipCode,
+    city: fieldValidators.city,
+    country: fieldValidators.country,
+    email: fieldValidators.email,
+    phone: fieldValidators.phone,
+    vatNumber: fieldValidators.stringOptional,
 });
 
 const ItemSchema = z.object({
-    name: z.string().min(1),
-    description: z.string().optional(),
-    quantity: z.coerce.number().min(1),
-    unitPrice: z.coerce.number().min(1),
-    total: z.coerce.number(),
+    name: fieldValidators.stringMin1,
+    description: fieldValidators.stringOptional,
+    quantity: fieldValidators.quantity,
+    unitPrice: fieldValidators.unitPrice,
+    total: fieldValidators.stringToNumber,
 });
 
 const PaymentInformationSchema = z.object({
-    bankName: z.string().min(1),
-    accountName: z.string().min(1),
-    accountNumber: z.string().min(1),
+    bankName: fieldValidators.stringMin1,
+    accountName: fieldValidators.stringMin1,
+    accountNumber: fieldValidators.stringMin1,
 });
 
 const DiscountDetailsSchema = z.object({
-    amount: z.coerce.number(),
-    amountType: z.string(),
+    amount: fieldValidators.stringToNumber,
+    amountType: fieldValidators.string,
 });
 
 const TaxDetailsSchema = z.object({
-    amount: z.coerce.number(),
-    taxID: z.string(),
-    amountType: z.string(),
+    amount: fieldValidators.stringToNumber,
+    taxID: fieldValidators.string,
+    amountType: fieldValidators.string,
 });
 
 const ShippingDetailsSchema = z.object({
-    cost: z.coerce.number(),
-    costType: z.string(),
+    cost: fieldValidators.stringToNumber,
+    costType: fieldValidators.string,
 });
 
 const InvoiceDetailsSchema = z.object({
-    invoiceLogo: z.string().optional(),
-    invoiceNumber: z.string().min(1),
-    invoiceDate: z
-        .date()
-        .transform((date) =>
-            new Date(date).toLocaleDateString(undefined, DATE_OPTIONS)
-        ),
-    dueDate: z
-        .date()
-        .transform((date) =>
-            new Date(date).toLocaleDateString(undefined, DATE_OPTIONS)
-        ),
-    purchaseOrderNumber: z.string().optional(),
-    currency: z.string(),
-    language: z.string(),
+    invoiceLogo: fieldValidators.stringOptional,
+    invoiceNumber: fieldValidators.stringMin1,
+    invoiceDate: fieldValidators.date,
+    dueDate: fieldValidators.date,
+    purchaseOrderNumber: fieldValidators.stringOptional,
+    currency: fieldValidators.string,
+    language: fieldValidators.string,
     items: z.array(ItemSchema),
+    paymentInformation: PaymentInformationSchema.optional(),
     taxDetails: TaxDetailsSchema.optional(),
     discountDetails: DiscountDetailsSchema.optional(),
     shippingDetails: ShippingDetailsSchema.optional(),
-    subTotal: z.coerce.number().transform((value) => {
-        return formatNumberWithCommas(value);
-    }),
-    totalAmount: z.coerce.number().transform((value) => {
-        return formatNumberWithCommas(value);
-    }),
-    totalAmountInWords: z.string(),
-    additionalNotes: z.string().optional(),
-    paymentTerms: z.string().min(1),
-    signature: z.string().optional(),
-    paymentInformation: PaymentInformationSchema.optional(),
+    subTotal: fieldValidators.numWithCommas,
+    totalAmount: fieldValidators.numWithCommas,
+    totalAmountInWords: fieldValidators.string,
+    additionalNotes: fieldValidators.stringOptional,
+    paymentTerms: fieldValidators.stringMin1,
+    signature: fieldValidators.stringOptional,
 });
 
 const InvoiceSchema = z.object({
