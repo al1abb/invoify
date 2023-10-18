f0d3112b3d5b8c2d9e1f2c463445cb0871f2b8fb
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-18 09:54:52 +0400

    refactor(logo): Update the logo files

--- app/components/navigation/BaseNavbar.tsx
+++ app/components/navigation/BaseNavbar.tsx
@@ -2,16 +2,13 @@ import React from "react";
 
 import Image from "next/image";
 
+import logo from "@/public/assets/img/invoify-logo.svg";
+
 const BaseNavbar = () => {
     return (
         <nav className="container flex justify-between items-center py-5 px-10 bg-white">
             <div>
-                <Image
-                    src={"/assets/img/invoify-logo.svg"}
-                    alt="Invoify Logo"
-                    width={200}
-                    height={100}
-                />
+                <Image src={logo} alt="Invoify Logo" width={200} height={150} />
 
                 <p>Generate invoices with ease.</p>
             </div>
