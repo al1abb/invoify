aea3f009dd27e19dbc44f0ac96d387c8399ac0e4
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-19 23:48:02 +0400

    refactor(ui): Minor changes in invoice actions style

--- app/components/invoice/InvoiceActions.tsx
+++ app/components/invoice/InvoiceActions.tsx
@@ -18,7 +18,7 @@ import {
 import { PdfViewer, BaseButton, SendPdfToEmailModal } from "@/app/components";
 
 // Lucide Icons
-import { Download, Eye, FileInput, Plus, Save } from "lucide-react";
+import { Download, Eye, FileInput, Mail, Plus, Save } from "lucide-react";
 
 // Variables
 import { FORM_DEFAULT_VALUES, FORM_FILL_VALUES } from "@/lib/variables";
@@ -56,20 +56,21 @@ const InvoiceActions = ({
 
     return (
         <div className="w-full xl:w-1/4">
-            <Card className="sticky top-0">
+            <Card className="sticky top-0 px-2">
                 <CardHeader>
                     <CardTitle>ACTIONS</CardTitle>
                     <CardDescription>Operations and preview</CardDescription>
                 </CardHeader>
-                <div className="flex flex-col px-2 gap-y-1">
+                <div className="flex flex-col flex-wrap gap-2">
                     {devEnv && (
                         <BaseButton
                             tooltipLabel="Form Test Fill"
                             variant="outline"
                             onClick={() => reset(FORM_FILL_VALUES)}
                             disabled={invoicePdfLoading}
+                            className="w-auto"
                         >
-                            Fill In the form (DEV)
+                            Fill in the form (DEV)
                         </BaseButton>
                     )}
                     <BaseButton
@@ -77,6 +78,7 @@ const InvoiceActions = ({
                         variant="outline"
                         onClick={newInvoice}
                         disabled={invoicePdfLoading}
+                        className="w-auto"
                     >
                         <Plus />
                         New Invoice
@@ -86,45 +88,52 @@ const InvoiceActions = ({
                         tooltipLabel="Generate your invoice"
                         loading={invoicePdfLoading}
                         loadingText="Generating your invoice"
+                        className="w-auto"
                     >
                         <FileInput />
                         <span>Generate PDF</span>
                     </BaseButton>
+                </div>
+                <div>
+                    {!invoicePdfLoading && invoicePdf.size != 0 && (
+                        <div>
+                            <PdfViewer pdfBlob={invoicePdf} />
+                            <div className="flex flex-row gap-2 py-3">
+                                <BaseButton
+                                    tooltipLabel="Preview invoice in new tab"
+                                    onClick={previewPdfInTab}
+                                    size="icon"
+                                >
+                                    <Eye />
+                                </BaseButton>
+                                <BaseButton
+                                    tooltipLabel="Download invoice PDF"
+                                    onClick={downloadPdf}
+                                    size="icon"
+                                >
+                                    <Download />
+                                </BaseButton>
+                                <BaseButton
+                                    tooltipLabel="Save invoice in website"
+                                    onClick={saveInvoice}
+                                    size="icon"
+                                >
+                                    <Save />
+                                </BaseButton>
 
-                    <div>
-                        {!invoicePdfLoading && invoicePdf.size != 0 && (
-                            <div>
-                                <PdfViewer pdfBlob={invoicePdf} />
-                                <div className="flex flex-row gap-2 py-3">
+                                <SendPdfToEmailModal
+                                    sendPdfToMail={sendPdfToMail}
+                                >
                                     <BaseButton
-                                        tooltipLabel="Preview invoice in new tab"
-                                        onClick={previewPdfInTab}
+                                        tooltipLabel="Send invoice PDF to mail"
                                         size="icon"
                                     >
-                                        <Eye />
+                                        <Mail />
                                     </BaseButton>
-                                    <BaseButton
-                                        tooltipLabel="Download invoice PDF"
-                                        onClick={downloadPdf}
-                                        size="icon"
-                                    >
-                                        <Download />
-                                    </BaseButton>
-                                    <BaseButton
-                                        tooltipLabel="Save invoice in website"
-                                        onClick={saveInvoice}
-                                        size="icon"
-                                    >
-                                        <Save />
-                                    </BaseButton>
-
-                                    <SendPdfToEmailModal
-                                        sendPdfToMail={sendPdfToMail}
-                                    />
-                                </div>
+                                </SendPdfToEmailModal>
                             </div>
-                        )}
-                    </div>
+                        </div>
+                    )}
                 </div>
             </Card>
         </div>
