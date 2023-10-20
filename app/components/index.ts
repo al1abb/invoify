14269111812c370d9cc2dc37dcb5f79ac127ccf9
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-20 18:04:06 +0400

    refactor: Components index file updated

--- app/components/index.ts
+++ app/components/index.ts
@@ -7,33 +7,43 @@ import InvoiceMain from "./invoice/InvoiceMain";
 import InvoiceForm from "./invoice/InvoiceForm";
 import InvoiceActions from "./invoice/InvoiceActions";
 
-// Invoice components
-import Items from "./invoice/components/Items";
-import SingleItem from "./invoice/components/SingleItem";
-import PaymentInformation from "./invoice/components/PaymentInformation";
-import InvoiceFooter from "./invoice/components/InvoiceFooter";
-import PdfViewer from "./invoice/components/PdfViewer";
+//* Invoice components
+// --- Form
+// ---/--- Sections
+import BillFromSection from "./invoice/components/form/sections/BillFromSection";
+import BillToSection from "./invoice/components/form/sections/BillToSection";
+import InvoiceDetails from "./invoice/components/form/sections/InvoiceDetails";
+import Items from "./invoice/components/form/sections/Items";
+import PaymentInformation from "./invoice/components/form/sections/PaymentInformation";
+import InvoiceFooter from "./invoice/components/form/sections/InvoiceFooter";
+
+// ---/--- Components
+import SingleItem from "./invoice/components/form/SingleItem";
+
+// --- Actions
+import PdfViewer from "./invoice/components/actions/PdfViewer";
 
 // Custom Selectors
 import CurrencySelector from "./form-fields/CurrencySelector";
-import SavedInvoiceSelector from "./invoice/components/SavedInvoiceSelector";
+import SavedInvoiceSelector from "./invoice/components/form/SavedInvoiceSelector";
 
 // Form fields
 import InputFormField from "./form-fields/InputFormField";
 import DatePickerFormField from "./form-fields/DatePickerFormField";
 import FileFormField from "./form-fields/FileFormField";
 import ChargeInput from "./form-fields/ChargeInput";
+import CustomInputField from "./form-fields/CustomInputField";
 
 // Reusable components
 import BaseButton from "./reusables/BaseButton";
 
-// Modals
+//* Modals
 import SendPdfToEmailModal from "./modals/SendPdfToEmailModal";
 
 // --- Alerts
 import NewInvoiceAlert from "./modals/alerts/NewInvoiceAlert";
 
-// Templates
+//* Templates
 // --- Invoice templates
 import InvoiceTemplate from "./templates/invoice-pdf/InvoiceTemplate";
 
@@ -46,6 +56,9 @@ export {
     InvoiceMain,
     InvoiceForm,
     InvoiceActions,
+    BillFromSection,
+    BillToSection,
+    InvoiceDetails,
     Items,
     SingleItem,
     PaymentInformation,
@@ -57,6 +70,7 @@ export {
     DatePickerFormField,
     FileFormField,
     ChargeInput,
+    CustomInputField,
     BaseButton,
     SendPdfToEmailModal,
     NewInvoiceAlert,
