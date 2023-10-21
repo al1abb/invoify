7ba41597499f0f8897595d56c379206cd9a3e4fb
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-21 17:31:20 +0400

    refactor: Use generatePdfService in generate route

--- app/api/pdf/generate/route.ts
+++ app/api/pdf/generate/route.ts
@@ -1,45 +1,10 @@
-import puppeteer, { Page } from "puppeteer";
-
-// Templates
-import { InvoiceTemplate } from "@/app/components";
+import generatePdf from "@/app/services/pdf/generatePdfService";
 
 export async function POST(req: Request, res: Response) {
     try {
         const body = await req.json();
 
-        // Read the HTML template from a React component
-        const htmlTemplate = InvoiceTemplate(body);
-
-        // Create a Puppeteer browser instance
-        const browser = await puppeteer.launch({
-            args: ["--no-sandbox", "--disable-setuid-sandbox"],
-            headless: "new",
-        });
-
-        const page: Page = await browser.newPage();
-
-        // Set the HTML content of the page
-        await page.setContent(await htmlTemplate);
-
-        // Generate the PDF
-        const pdf: Buffer = await page.pdf({
-            format: "A4", // You can change the page format here
-            printBackground: true,
-        });
-
-        // Close the Puppeteer browser
-        await browser.close();
-
-        // Create a Blob from the PDF data
-        const pdfBlob = new Blob([pdf], { type: "application/pdf" });
-
-        const response = new Response(pdfBlob, {
-            headers: {
-                "Content-Type": "application/pdf",
-                "Content-Disposition": "inline; filename=invoice.pdf",
-            },
-            status: 200,
-        });
+        const response = await generatePdf(body);
 
         return response;
     } catch (error) {
