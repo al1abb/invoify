45b1279d21e82bfc7df9772e923624954901fcf1
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-27 22:59:06 +0400

    fix: Gap issue in single item total

--- app/components/invoice/components/form/components/SingleItem.tsx
+++ app/components/invoice/components/form/components/SingleItem.tsx
@@ -145,7 +145,7 @@ const SingleItem = ({
                     )}
                 />
 
-                <div className="flex flex-col justify-between">
+                <div className="flex flex-col gap-2">
                     <div>
                         <Label>Total</Label>
                     </div>
