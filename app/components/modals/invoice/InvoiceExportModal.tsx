389cb2ced93faf5f1e41f016b3bbc62f8f55c00f
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-27 21:32:18 +0400

    feat: New invoice export modal button
    Includes initial JSON export button that exports invoice as JSON file

---
+++ app/components/modals/invoice/InvoiceExportModal.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { useState } from "react";
+
+// ShadCn
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+
+// Components
+import { BaseButton } from "@/app/components";
+
+// Context
+import { useInvoiceContext } from "@/app/contexts/InvoiceContext";
+
+// Icons
+import { Import } from "lucide-react";
+
+type InvoiceExportModalProps = {};
+
+const InvoiceExportModal = (props: InvoiceExportModalProps) => {
+    const [open, setOpen] = useState(false);
+
+    const { invoicePdfLoading, exportInvoice } = useInvoiceContext();
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <BaseButton
+                    variant="outline"
+                    tooltipLabel="Open load invoice menu"
+                    className="flex gap-2"
+                    disabled={invoicePdfLoading}
+                >
+                    <Import />
+                    Export invoice
+                </BaseButton>
+            </DialogTrigger>
+
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Export the invoice</DialogTitle>
+                    <DialogDescription>
+                        Please select export option for your invoice
+                    </DialogDescription>
+                </DialogHeader>
+
+                {/* Export options here */}
+
+                <BaseButton
+                    tooltipLabel="Export Invoice as JSON"
+                    disabled={invoicePdfLoading}
+                    onClick={() => exportInvoice("JSON")}
+                >
+                    Export as JSON
+                </BaseButton>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default InvoiceExportModal;
