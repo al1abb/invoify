11d61718ecefa6ad7e413b7feb171784c130d98d
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-28 16:51:05 +0400

    refactor: Update final wizard step validation rule

--- app/components/invoice/components/form/components/WizardProgress.tsx
+++ app/components/invoice/components/form/components/WizardProgress.tsx
@@ -34,7 +34,10 @@ const WizardProgress = ({ wizard }: WizardProgressProps) => {
 
     const step3Valid = !errors.details?.items;
     const step4Valid = !errors.details?.paymentInformation;
-    const step5Valid = !errors.details?.paymentTerms;
+    const step5Valid =
+        !errors.details?.paymentTerms &&
+        !errors.details?.subTotal &&
+        !errors.details?.totalAmount;
 
     const returnButtonVariant = (step: WizardStepType) => {
         if (!step.isValid) {
