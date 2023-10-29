e81c121115e5f39f065f0a6fd985869468219726
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-29 22:53:53 +0400

    refactor: Modify export api route to accept JSON and CSV
    Json2csv library is utilized to parse json to csv

--- app/api/pdf/export/route.ts
+++ app/api/pdf/export/route.ts
@@ -1,16 +1,45 @@
-export async function POST(req: Request, res: Response) {
+import { NextRequest } from "next/server";
+
+// JSON2CSV
+import { AsyncParser } from "@json2csv/node";
+
+// Types
+import { ExportTypes } from "@/app/types/types";
+
+export async function POST(req: NextRequest, res: Response) {
+    const format = req.nextUrl.searchParams.get("format");
+
+    const body = await req.json();
+
+    const jsonData = JSON.stringify(body);
+
     try {
-        const body = await req.json();
+        switch (format) {
+            case ExportTypes.JSON:
+                return new Response(jsonData, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Content-Disposition":
+                            "attachment; filename=invoice.json",
+                    },
+                    status: 200,
+                });
+            case ExportTypes.CSV:
+                console.log("DATA", body);
 
-        const jsonData = JSON.stringify(body);
+                //? Can pass specific fields to async parser. Empty = All
+                const parser = new AsyncParser();
+                const csv = await parser.parse(body).promise();
 
-        return new Response(jsonData, {
-            headers: {
-                "Content-Type": "application/json",
-                "Content-Disposition": "attachment; filename=data.json",
-            },
-            status: 200,
-        });
+                console.log(csv);
+                return new Response(csv, {
+                    headers: {
+                        "Content-Type": "text/csv",
+                        "Content-Disposition":
+                            "attachment; filename=invoice.csv",
+                    },
+                });
+        }
     } catch (error) {
         console.error(error);
 
