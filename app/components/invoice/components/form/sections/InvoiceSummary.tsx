ccb41b9dfc3b2cfbd260dfda7108401f1a3c73cb
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-29 14:34:02 +0400

    refactor: Rename invoice footer to summary

---
+++ app/components/invoice/components/form/sections/InvoiceSummary.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+// RHF
+import { useFormContext } from "react-hook-form";
+
+// ShadCn
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+// Components
+import { Charges, SignatureModal } from "@/app/components";
+
+// Types
+import { InvoiceType } from "@/app/types/types";
+
+type InvoiceSummaryProps = {};
+
+const InvoiceSummary = (props: InvoiceSummaryProps) => {
+    const { control, setValue } = useFormContext<InvoiceType>();
+
+    return (
+        <>
+            <Label className="text-xl font-semibold">Summary:</Label>
+            <div className="flex flex-wrap justify-around gap-y-10">
+                {/* Additional notes & Payment terms */}
+                <div className="flex flex-col gap-3">
+                    <SignatureModal />
+
+                    {/* Turn these into custom textarea components */}
+                    <FormField
+                        control={control}
+                        name="details.additionalNotes"
+                        render={({ field }) => (
+                            <FormItem>
+                                <Label>Additional Notes</Label>
+                                <div className="flex justify-between gap-5 items-center text-sm">
+                                    <div>
+                                        <FormControl>
+                                            <Textarea
+                                                {...field}
+                                                placeholder="Your additional notes"
+                                                className="w-[15rem] h-0"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </div>
+                                </div>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="details.paymentTerms"
+                        render={({ field }) => (
+                            <FormItem>
+                                <Label>Payment terms</Label>
+                                <div className="flex justify-between gap-5 items-center text-sm">
+                                    <div>
+                                        <FormControl>
+                                            <Textarea
+                                                {...field}
+                                                placeholder="Ex: Net 30"
+                                                className="w-[15rem] h-0"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </div>
+                                </div>
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <Charges />
+            </div>
+        </>
+    );
+};
+
+export default InvoiceSummary;
