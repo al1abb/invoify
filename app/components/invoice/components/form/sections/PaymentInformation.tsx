f3ce2b8277b5274caa96f6a6db28540876bb1c1d
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-29 13:00:08 +0400

    refactor: Replace form fields with inputs in payment info

--- app/components/invoice/components/form/sections/PaymentInformation.tsx
+++ app/components/invoice/components/form/sections/PaymentInformation.tsx
@@ -5,15 +5,11 @@ import React from "react";
 // RHF
 import { useFormContext } from "react-hook-form";
 
-// Shadcn
-import {
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+// ShadCn
 import { Label } from "@/components/ui/label";
+
+// Components
+import { InputFormField } from "@/app/components";
 
 type PaymentInformationProps = {};
 
@@ -25,65 +21,26 @@ const PaymentInformation = (props: PaymentInformationProps) => {
                 Payment Information:
             </Label>
             <div className="flex flex-wrap gap-10 mt-5">
-                <FormField
+                <InputFormField
                     control={control}
                     name="details.paymentInformation.bankName"
-                    render={({ field }) => (
-                        <FormItem>
-                            <Label>Bank name</Label>
-                            <div className="flex justify-between gap-5 items-center text-sm">
-                                <div>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            placeholder="Bank name"
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </div>
-                            </div>
-                        </FormItem>
-                    )}
+                    label="Bank name"
+                    placeholder="Bank name"
+                    vertical
                 />
-                <FormField
+                <InputFormField
                     control={control}
                     name="details.paymentInformation.accountName"
-                    render={({ field }) => (
-                        <FormItem>
-                            <Label>Account name</Label>
-                            <div className="flex justify-between gap-5 items-center text-sm">
-                                <div>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            placeholder="Account name"
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </div>
-                            </div>
-                        </FormItem>
-                    )}
+                    label="Account name"
+                    placeholder="Account name"
+                    vertical
                 />
-                <FormField
+                <InputFormField
                     control={control}
                     name="details.paymentInformation.accountNumber"
-                    render={({ field }) => (
-                        <FormItem>
-                            <Label>Account number</Label>
-                            <div className="flex justify-between gap-5 items-center text-sm">
-                                <div>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            placeholder="Account number"
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </div>
-                            </div>
-                        </FormItem>
-                    )}
+                    label="Account number"
+                    placeholder="Account number"
+                    vertical
                 />
             </div>
         </>
