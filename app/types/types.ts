f0db9a42ff583d639fdb055416830ba60a25425c
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-30 12:29:11 +0400

    refactor: Remove redundant types related to form

--- app/types/types.ts
+++ app/types/types.ts
@@ -2,14 +2,7 @@
 import z from "zod";
 
 // RHF
-import {
-    Control,
-    FieldPath,
-    UseFormGetValues,
-    UseFormReset,
-    UseFormReturn,
-    UseFormSetValue,
-} from "react-hook-form";
+import { FieldPath, UseFormReturn } from "react-hook-form";
 
 // Zod schemas
 import { InvoiceSchema, ItemSchema } from "@/lib/schemas";
@@ -17,15 +10,11 @@ import { InvoiceSchema, ItemSchema } from "@/lib/schemas";
 // Form types
 export type InvoiceType = z.infer<typeof InvoiceSchema>;
 export type ItemType = z.infer<typeof ItemSchema>;
+export type FormType = UseFormReturn<InvoiceType>;
+export type NameType = FieldPath<InvoiceType>;
 export type CurrencyType = {
     [currencyCode: string]: string;
 };
-export type FormType = UseFormReturn<InvoiceType>;
-export type ControlType = Control<any>;
-export type NameType = FieldPath<InvoiceType>;
-export type GetValuesType = UseFormGetValues<InvoiceType>;
-export type UseFormSetValueType = UseFormSetValue<InvoiceType>;
-export type UseFormResetType = UseFormReset<InvoiceType>;
 
 // Signature types
 export type SignatureColor = {
