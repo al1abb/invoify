ff032da09e1c1cbf1c7cb69fb0034b8273696a44
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-30 20:18:10 +0400

    feat: New form textarea component

---
+++ app/components/form-fields/FormTextarea.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React from "react";
+
+// RHF
+import { useFormContext } from "react-hook-form";
+
+// ShadCn
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Textarea, TextareaProps } from "@/components/ui/textarea";
+
+// Types
+import { InvoiceType } from "@/app/types/types";
+
+type FormTextareaProps = {
+    name: string;
+    label?: string;
+    labelHelper?: string;
+    placeholder?: string;
+} & TextareaProps;
+
+const FormTextarea = ({
+    name,
+    label,
+    labelHelper,
+    placeholder,
+    ...props
+}: FormTextareaProps) => {
+    const { control } = useFormContext<InvoiceType>();
+    return (
+        <FormField
+            control={control}
+            name="details.additionalNotes"
+            render={({ field }) => (
+                <FormItem>
+                    {label && <FormLabel>{`${label}:`}</FormLabel>}
+                    {labelHelper && (
+                        <span className="text-xs"> {labelHelper}</span>
+                    )}
+                    <div className="flex justify-between gap-5 items-center text-sm">
+                        <div>
+                            <FormControl>
+                                <Textarea
+                                    {...field}
+                                    placeholder={placeholder}
+                                    className="w-[15rem] h-0"
+                                    {...props}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </div>
+                    </div>
+                </FormItem>
+            )}
+        />
+    );
+};
+
+export default FormTextarea;
