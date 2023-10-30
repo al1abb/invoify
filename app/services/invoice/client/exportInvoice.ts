e54a28c36d3af990317592f6bce1636378ade0fe
Ali Abbasov <ali3abbasov@gmail.com>
2023-10-30 09:22:55 +0400

    refactor: Add client side export invoice service
    This service implements the fetch call on client side

--- app/services/invoice/client/exportInvoice.ts
+++ app/services/invoice/client/exportInvoice.ts
@@ -2,10 +2,20 @@
 import { EXPORT_INVOICE_API } from "@/lib/variables";
 
 // Types
-import { InvoiceType } from "@/app/types/types";
+import { ExportTypes, InvoiceType } from "@/app/types/types";
 
+/**
+ * Export an invoice by sending a POST request to the server and initiating the download.
+ *
+ * @param {ExportTypes} exportAs - The format in which to export the invoice (e.g., JSON, CSV).
+ * @param {InvoiceType} formValues - The invoice form data to be exported.
+ *
+ * @throws {Error} If there is an error during the export process.
+ *
+ * @returns {Promise<void>} A promise that resolves when the export is completed.
+ */
 export const exportInvoice = async (
-    exportAs: string,
+    exportAs: ExportTypes,
     formValues: InvoiceType
 ) => {
     return fetch(`${EXPORT_INVOICE_API}?format=${exportAs}`, {
