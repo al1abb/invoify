c217eaeaa087baa562c9bd692e5a615e2bfafc48
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-03 17:13:20 +0400

    add: Optional server component translation config

---
+++ app/i18n/i18n.ts
@@ -0,0 +1,8 @@
+// ? NOTE:
+// Below code is used for server component i18n approach logic
+
+// import { getRequestConfig } from "next-intl/server";
+
+// export default getRequestConfig(async ({ locale }) => ({
+//     messages: (await import(`./locales/${locale}.json`)).default,
+// }));
