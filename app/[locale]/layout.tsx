c8d80d0eab31c40a54d82e610014d2ed2975334a
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-03 17:14:22 +0400

    refactor: Move root layout to /locale

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,97 @@
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+// Fonts
+import {
+    alexBrush,
+    dancingScript,
+    greatVibes,
+    outfit,
+    parisienne,
+} from "@/app/fonts/fonts";
+
+// Favicon
+import Favicon from "@/public/assets/favicon/favicon.ico";
+
+// ShadCn
+import { Toaster } from "@/components/ui/toaster";
+
+// Components
+import { BaseNavbar, BaseFooter } from "@/app/components";
+
+// Context
+import Providers from "@/app/contexts/Providers";
+
+// Next Intl
+import { NextIntlClientProvider } from "next-intl";
+
+// Variables
+import { LOCALES } from "@/lib/variables";
+
+export const metadata: Metadata = {
+    title: "Invoify | Free Invoice Generator",
+    description: "Invoice generating application",
+    icons: [{ rel: "icon", url: Favicon.src }],
+    keywords: [
+        "invoice",
+        "invoice generating",
+        "invoice generator",
+        "free invoice generator",
+        "invoice app",
+        "invoice generator app",
+    ],
+    viewport: "width=device-width, initial-scale=1",
+    robots: {
+        index: true,
+        follow: true,
+    },
+};
+
+export function generateStaticParams() {
+    const locales = LOCALES.map((locale) => locale.code);
+    return locales;
+}
+
+export default async function LocaleLayout({
+    children,
+    params: { locale },
+}: {
+    children: React.ReactNode;
+    params: { locale: string };
+}) {
+    // ? NOTE:
+    // Below code is used for server component i18n approach logic
+
+    // Validate that the incoming `locale` parameter is valid
+    // If not, return default not found page
+    // const isValidLocale = locales.some((cur) => cur === locale);
+    // if (!isValidLocale) notFound();
+
+    let messages;
+    try {
+        messages = (await import(`@/app/i18n/locales/${locale}.json`)).default;
+    } catch (error) {
+        notFound();
+    }
+
+    return (
+        <html lang={locale}>
+            <body
+                className={`${outfit.className} ${dancingScript.variable} ${parisienne.variable} ${greatVibes.variable} ${alexBrush.variable} antialiased`}
+            >
+                <NextIntlClientProvider locale={locale} messages={messages}>
+                    <Providers>
+                        <BaseNavbar />
+
+                        <div className="flex flex-col">{children}</div>
+
+                        <BaseFooter />
+
+                        {/* Toast component */}
+                        <Toaster />
+                    </Providers>
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    );
+}
