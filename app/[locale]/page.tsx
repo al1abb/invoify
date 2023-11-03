facc8926d88101fa3852cd38409669b57b1b4234
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-03 17:16:20 +0400

    refactor: Move invoice page to /locale

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,9 @@
+import { InvoiceMain } from "@/app/components";
+
+export default function Home() {
+    return (
+        <main className="lg:container md:container sm:container-lg py-10 sm:py-4 xs:container-lg">
+            <InvoiceMain />
+        </main>
+    );
+}
