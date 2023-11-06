6eb3e530db949d55a6bb6c992862ec8b73d9097b
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-06 15:53:00 +0400

    refactor: Update pdf viewer styles, add print pdf button

--- app/components/invoice/components/actions/PdfViewer.tsx
+++ app/components/invoice/components/actions/PdfViewer.tsx
@@ -10,27 +10,33 @@ import { BaseButton, SendPdfToEmailModal } from "@/app/components";
 import { useInvoiceContext } from "@/app/contexts/InvoiceContext";
 
 // Icons
-import { Download, Eye, Mail, Save } from "lucide-react";
+import { Download, Eye, Mail, Printer, Save } from "lucide-react";
 
 type PdfViewerProps = {
     pdfBlob: Blob;
 };
 
 const PdfViewer = ({ pdfBlob }: PdfViewerProps) => {
-    const { previewPdfInTab, downloadPdf, saveInvoice, sendPdfToMail } =
-        useInvoiceContext();
+    const {
+        previewPdfInTab,
+        downloadPdf,
+        printPdf,
+        saveInvoice,
+        sendPdfToMail,
+    } = useInvoiceContext();
 
     const pdfUrl = useMemo(() => {
         return window.URL.createObjectURL(pdfBlob);
     }, [pdfBlob]);
     return (
-        <>
-            <p className="text-xl font-semibold my-2">PDF View</p>
-            <div className="flex flex-row gap-2 py-3">
+        <div className="my-5">
+            <p className="text-xl font-semibold">PDF View</p>
+            <div className="flex flex-wrap gap-x-2 py-1">
                 <BaseButton
                     tooltipLabel="Preview invoice in new tab"
                     onClick={previewPdfInTab}
                     size="icon"
+                    variant={"outline"}
                 >
                     <Eye />
                 </BaseButton>
@@ -38,13 +44,25 @@ const PdfViewer = ({ pdfBlob }: PdfViewerProps) => {
                     tooltipLabel="Download invoice PDF"
                     onClick={downloadPdf}
                     size="icon"
+                    variant={"outline"}
                 >
                     <Download />
                 </BaseButton>
+
+                <BaseButton
+                    tooltipLabel="Print invoice"
+                    onClick={printPdf}
+                    size="icon"
+                    variant={"outline"}
+                >
+                    <Printer />
+                </BaseButton>
+
                 <BaseButton
                     tooltipLabel="Save invoice in website"
                     onClick={saveInvoice}
                     size="icon"
+                    variant={"outline"}
                 >
                     <Save />
                 </BaseButton>
@@ -53,6 +71,7 @@ const PdfViewer = ({ pdfBlob }: PdfViewerProps) => {
                     <BaseButton
                         tooltipLabel="Send invoice PDF to mail"
                         size="icon"
+                        variant={"outline"}
                     >
                         <Mail />
                     </BaseButton>
@@ -64,7 +83,7 @@ const PdfViewer = ({ pdfBlob }: PdfViewerProps) => {
                     src={`${pdfUrl}#toolbar=0`}
                 ></iframe>
             </AspectRatio>
-        </>
+        </div>
     );
 };
 
