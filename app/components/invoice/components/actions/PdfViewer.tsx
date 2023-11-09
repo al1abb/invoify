af4b236fa288d04e78441832b54fe24e77fd44c8
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-09 15:28:14 +0400

    feat!: Add live preview to pdf viewer
    Users can now see the invoice content update as they fill in the form

--- app/components/invoice/components/actions/PdfViewer.tsx
+++ app/components/invoice/components/actions/PdfViewer.tsx
@@ -1,10 +1,17 @@
-import { useMemo } from "react";
+"use client";
+
+// RHF
+import { useFormContext } from "react-hook-form";
 
 // ShadCn components
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 // Components
-import { BaseButton, SendPdfToEmailModal } from "@/app/components";
+import {
+    BaseButton,
+    InvoiceTemplate,
+    SendPdfToEmailModal,
+} from "@/app/components";
 
 // Contexts
 import { useInvoiceContext } from "@/app/contexts/InvoiceContext";
@@ -12,12 +19,17 @@ import { useInvoiceContext } from "@/app/contexts/InvoiceContext";
 // Icons
 import { Download, Eye, Mail, Printer, Save } from "lucide-react";
 
-type PdfViewerProps = {
-    pdfBlob: Blob;
-};
+import { useDebounce } from "use-debounce";
 
-const PdfViewer = ({ pdfBlob }: PdfViewerProps) => {
+// ! Old passed props type
+// type PdfViewerProps = {
+//     pdfBlob: Blob;
+// };
+
+const PdfViewer = ({}) => {
     const {
+        invoicePdf,
+        pdfUrl,
         previewPdfInTab,
         downloadPdf,
         printPdf,
@@ -25,64 +37,86 @@ const PdfViewer = ({ pdfBlob }: PdfViewerProps) => {
         sendPdfToMail,
     } = useInvoiceContext();
 
-    const pdfUrl = useMemo(() => {
-        return window.URL.createObjectURL(pdfBlob);
-    }, [pdfBlob]);
+    // ? Uncomment to enable live preview
+    const { watch } = useFormContext();
+
+    const [debouncedWatch] = useDebounce(watch, 2000);
+    const formValues = debouncedWatch();
+
+    // ? This was moved to context to fix window not defined error
+    // const pdfUrl = useMemo(() => {
+    //     return window.URL.createObjectURL(invoicePdf);
+    // }, [invoicePdf]);
     return (
         <div className="my-5">
-            <p className="text-xl font-semibold">PDF View</p>
-            <div className="flex flex-wrap gap-x-2 py-1">
-                <BaseButton
-                    tooltipLabel="Preview invoice in new tab"
-                    onClick={previewPdfInTab}
-                    size="icon"
-                    variant={"outline"}
-                >
-                    <Eye />
-                </BaseButton>
-                <BaseButton
-                    tooltipLabel="Download invoice PDF"
-                    onClick={downloadPdf}
-                    size="icon"
-                    variant={"outline"}
-                >
-                    <Download />
-                </BaseButton>
+            {invoicePdf.size == 0 && (
+                <>
+                    <p className="text-xl font-semibold">Live preview:</p>
+                    <InvoiceTemplate
+                        sender={formValues?.sender}
+                        receiver={formValues?.receiver}
+                        details={formValues?.details}
+                    />
+                </>
+            )}
 
-                <BaseButton
-                    tooltipLabel="Print invoice"
-                    onClick={printPdf}
-                    size="icon"
-                    variant={"outline"}
-                >
-                    <Printer />
-                </BaseButton>
+            {invoicePdf.size != 0 && (
+                <>
+                    <p className="text-xl font-semibold">PDF View</p>
+                    <div className="flex flex-wrap gap-x-2 py-1">
+                        <BaseButton
+                            tooltipLabel="Preview invoice in new tab"
+                            onClick={previewPdfInTab}
+                            size="icon"
+                            variant={"outline"}
+                        >
+                            <Eye />
+                        </BaseButton>
+                        <BaseButton
+                            tooltipLabel="Download invoice PDF"
+                            onClick={downloadPdf}
+                            size="icon"
+                            variant={"outline"}
+                        >
+                            <Download />
+                        </BaseButton>
 
-                <BaseButton
-                    tooltipLabel="Save invoice in website"
-                    onClick={saveInvoice}
-                    size="icon"
-                    variant={"outline"}
-                >
-                    <Save />
-                </BaseButton>
+                        <BaseButton
+                            tooltipLabel="Print invoice"
+                            onClick={printPdf}
+                            size="icon"
+                            variant={"outline"}
+                        >
+                            <Printer />
+                        </BaseButton>
 
-                <SendPdfToEmailModal sendPdfToMail={sendPdfToMail}>
-                    <BaseButton
-                        tooltipLabel="Send invoice PDF to mail"
-                        size="icon"
-                        variant={"outline"}
-                    >
-                        <Mail />
-                    </BaseButton>
-                </SendPdfToEmailModal>
-            </div>
-            <AspectRatio ratio={1 / 1.4}>
-                <iframe
-                    className="h-full w-full"
-                    src={`${pdfUrl}#toolbar=0`}
-                ></iframe>
-            </AspectRatio>
+                        <BaseButton
+                            tooltipLabel="Save invoice in website"
+                            onClick={saveInvoice}
+                            size="icon"
+                            variant={"outline"}
+                        >
+                            <Save />
+                        </BaseButton>
+
+                        <SendPdfToEmailModal sendPdfToMail={sendPdfToMail}>
+                            <BaseButton
+                                tooltipLabel="Send invoice PDF to mail"
+                                size="icon"
+                                variant={"outline"}
+                            >
+                                <Mail />
+                            </BaseButton>
+                        </SendPdfToEmailModal>
+                    </div>
+                    <AspectRatio ratio={1 / 1.4}>
+                        <iframe
+                            className="h-full w-full"
+                            src={`${pdfUrl}#toolbar=0`}
+                        ></iframe>
+                    </AspectRatio>
+                </>
+            )}
         </div>
     );
 };
