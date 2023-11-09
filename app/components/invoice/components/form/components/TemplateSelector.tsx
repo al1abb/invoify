8a5e9c24f90147d0403878baa248acd9457f327b
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-09 15:26:52 +0400

    feat: Initial template selector component

--- app/components/invoice/components/form/components/TemplateSelector.tsx
+++ app/components/invoice/components/form/components/TemplateSelector.tsx
@@ -6,14 +6,50 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 
 type TemplateSelectorProps = {};
 
 const TemplateSelector = ({}: TemplateSelectorProps) => {
+    const templates = [
+        {
+            name: "Template 1",
+            description: "Template 1 description",
+            img: "img src",
+        },
+        {
+            name: "Template 2",
+            description: "Template 2 description",
+            img: "img src",
+        },
+        {
+            name: "Template 3",
+            description: "Template 3 description",
+            img: "img src",
+        },
+    ];
     return (
         <>
             <div>
-                <p>Choose invoice template</p>
+                <Label>Choose invoice template</Label>
+
+                <div>
+                    <Card>
+                        <CardHeader>
+                            <p>Template 1</p>
+                            <CardDescription>
+                                Template 1 description
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            {templates.map((template, idx) => (
+                                <>
+                                    <p>{template.name}</p>
+                                </>
+                            ))}
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </>
     );
