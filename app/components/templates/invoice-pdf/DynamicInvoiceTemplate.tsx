53a429b2b9316421357352f3dc0223f25ec582f1
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-11 19:52:10 +0400

    refactor: Externalize dynamic invoice template

---
+++ app/components/templates/invoice-pdf/DynamicInvoiceTemplate.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React, { Suspense, useEffect, useState } from "react";
+
+// ShadCn
+import { Skeleton } from "@/components/ui/skeleton";
+
+// Types
+import { InvoiceType } from "@/app/types/types";
+
+const DynamicInvoiceTemplateSkeleton = () => {
+    return <Skeleton className="w-full h-full" />;
+};
+
+const DynamicInvoiceTemplate = (props: InvoiceType) => {
+    // State to store the dynamically imported component
+    const [DynamicTemplate, setDynamicTemplate] =
+        useState<React.ComponentType<InvoiceType> | null>(null);
+
+    // Dynamic template component name
+    const templateName = `InvoiceTemplate${props.details.pdfTemplate}`;
+
+    // * This useEffect fires only when template changes
+    useEffect(() => {
+        // Import the component dynamically
+        const importPromise = import(
+            `@/app/components/templates/invoice-pdf/${templateName}`
+        );
+
+        importPromise
+            .then((module) => {
+                setDynamicTemplate(() => module.default);
+            })
+            .catch((error) => {
+                console.error(
+                    `Error importing template ${templateName}: ${error}`
+                );
+            });
+    }, [templateName]);
+
+    if (!DynamicTemplate) {
+        // Loading state or fallback component
+        return <DynamicInvoiceTemplateSkeleton />;
+    }
+
+    return (
+        <Suspense fallback={<DynamicInvoiceTemplateSkeleton />}>
+            <DynamicTemplate {...props} />
+        </Suspense>
+    );
+};
+
+export default DynamicInvoiceTemplate;
