5bd5c0bcd1687a1442834e8098231ae418558304
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-12 18:05:56 +0400

    feat: Added components to template array

--- app/components/invoice/components/form/components/TemplateSelector.tsx
+++ app/components/invoice/components/form/components/TemplateSelector.tsx
@@ -13,12 +13,15 @@ import {
     CardContent,
     CardDescription,
     CardHeader,
-    CardTitle,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 
 // Components
-import { BaseButton } from "@/app/components";
+import {
+    BaseButton,
+    InvoiceTemplate1,
+    InvoiceTemplate2,
+} from "@/app/components";
 
 // Template images
 import template1 from "@/public/assets/img/invoice-1-example.png";
@@ -29,19 +32,22 @@ import { InvoiceType } from "@/app/types/types";
 type TemplateSelectorProps = {};
 
 const TemplateSelector = ({}: TemplateSelectorProps) => {
-    const { setValue } = useFormContext<InvoiceType>();
+    const { watch, setValue } = useFormContext<InvoiceType>();
+    const formValues = watch();
     const templates = [
         {
             id: 1,
             name: "Template 1",
             description: "Template 1 description",
             img: template1,
+            component: <InvoiceTemplate1 {...formValues} />,
         },
         {
             id: 2,
             name: "Template 2",
             description: "Second template",
             img: template1,
+            component: <InvoiceTemplate2 {...formValues} />,
         },
     ];
     return (
@@ -54,7 +60,7 @@ const TemplateSelector = ({}: TemplateSelectorProps) => {
                         <CardHeader>
                             <p>Templates</p>
                             <CardDescription>
-                                Select one of the templates
+                                Select one of the predefined templates
                             </CardDescription>
                         </CardHeader>
                         <CardContent className="">
@@ -62,7 +68,7 @@ const TemplateSelector = ({}: TemplateSelectorProps) => {
                                 {templates.map((template, idx) => (
                                     <div
                                         key={idx}
-                                        className="flex flex-col flex-shrink-0 mr-4"
+                                        className="flex flex-col flex-shrink-0 mr-4 my-5 gap-y-3"
                                     >
                                         <p>{template.name}</p>
 
@@ -73,6 +79,7 @@ const TemplateSelector = ({}: TemplateSelectorProps) => {
                                                 width={300}
                                                 height={700}
                                             />
+                                            {/* {template.component} */}
                                         </div>
 
                                         <BaseButton
