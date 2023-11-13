546f617e0e8e2342d44d1f56af543bbb659f2c75
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-13 20:28:58 +0400

    feat: Add template page to display templates for dev only

---
+++ app/[locale]/template/[id]/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+
+// Next
+import dynamic from "next/dynamic";
+
+// RHF
+import { useFormContext } from "react-hook-form";
+
+// Types
+import { InvoiceType } from "@/app/types/types";
+
+type ViewTemplatePageProps = {
+    params: { id: string };
+};
+
+const ViewTemplate = ({ params }: ViewTemplatePageProps) => {
+    const templateNumber = params.id;
+
+    const DynamicComponent = dynamic<InvoiceType>(
+        () =>
+            import(
+                `@/app/components/templates/invoice-pdf/InvoiceTemplate${templateNumber}`
+            )
+    );
+
+    const { getValues } = useFormContext();
+    const formValues = getValues();
+
+    return (
+        <div className="container">
+            <DynamicComponent
+                sender={formValues.sender}
+                receiver={formValues.receiver}
+                details={formValues.details}
+            />
+        </div>
+    );
+};
+
+export default ViewTemplate;
