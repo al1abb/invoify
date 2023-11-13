b36b801eb6b5611a3ada19aad7d977db47d69927
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-13 12:58:34 +0400

    refactor: Make logo a link that redirects to homepage

--- app/components/navigation/BaseNavbar.tsx
+++ app/components/navigation/BaseNavbar.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useMemo } from "react";
 
+// Next
+import Link from "next/link";
 import Image from "next/image";
 
 // Assets
@@ -9,25 +11,21 @@ import Logo from "@/public/assets/img/invoify-logo.svg";
 import { DevDebug, LanguageSelector, ThemeSwitcher } from "@/app/components";
 
 const BaseNavbar = () => {
+    const devEnv = useMemo(() => {
+        return process.env.NODE_ENV === "development";
+    }, []);
+
     return (
-        <nav className="container flex justify-between items-center py-5 px-10">
-            <div>
+        <nav className="container flex flex-wrap gap-5 justify-between items-center py-5 px-10">
+            <Link href={"/"}>
                 <Image src={Logo} alt="Invoify Logo" width={200} height={150} />
 
                 <p>Generate invoices with ease.</p>
-            </div>
-
-            <div>
-                <DevDebug />
-            </div>
-
-            <div>
-                <LanguageSelector />
-            </div>
-
-            <div>
-                <ThemeSwitcher />
-            </div>
+            </Link>
+            {/* ? DEV Only */}
+            {devEnv && <DevDebug />}
+            <LanguageSelector />
+            <ThemeSwitcher />
         </nav>
     );
 };
