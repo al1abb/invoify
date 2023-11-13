388c3684348db49207af9b46c6783290fc14c970
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-13 20:29:56 +0400

    refactor: Reorder main element classes

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { InvoiceMain } from "@/app/components";
 
 export default function Home() {
     return (
-        <main className="lg:container md:container sm:container-lg py-10 sm:py-4 xs:container-lg">
+        <main className="py-10 lg:container md:container sm:container-lg sm:py-4 xs:container-lg">
             <InvoiceMain />
         </main>
     );
