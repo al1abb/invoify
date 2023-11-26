b19d032a4c25a99684197f5bf212dc1956947c50
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-26 13:11:22 +0400

    chore: Remove custom border radius from theme switch btn

--- app/components/reusables/ThemeSwitcher.tsx
+++ app/components/reusables/ThemeSwitcher.tsx
@@ -18,7 +18,6 @@ const ThemeSwitcher = ({}: Props) => {
         <>
             <BaseButton
                 size="icon"
-                className="rounded-full"
                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
             >
                 <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
