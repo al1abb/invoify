9c3122a8c4a3189981bf45a90de48b438c3baa70
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-03 17:44:26 +0400

    refactor(ui): Add bg colors and borders to navbar

--- app/components/navigation/BaseNavbar.tsx
+++ app/components/navigation/BaseNavbar.tsx
@@ -7,6 +7,9 @@ import Image from "next/image";
 // Assets
 import Logo from "@/public/assets/img/invoify-logo.svg";
 
+// ShadCn
+import { Card } from "@/components/ui/card";
+
 // Components
 import { DevDebug, LanguageSelector, ThemeSwitcher } from "@/app/components";
 
@@ -16,15 +19,24 @@ const BaseNavbar = () => {
     }, []);
 
     return (
-        <nav className="container flex flex-wrap justify-between items-center gap-5 px-10">
-            <Link href={"/"}>
-                <Image src={Logo} alt="Invoify Logo" width={190} height={100} />
-            </Link>
-            {/* ? DEV Only */}
-            {devEnv && <DevDebug />}
-            <LanguageSelector />
-            <ThemeSwitcher />
-        </nav>
+        <header className="lg:container z-[99]">
+            <nav>
+                <Card className="flex flex-wrap justify-between items-center px-5 gap-5">
+                    <Link href={"/"}>
+                        <Image
+                            src={Logo}
+                            alt="Invoify Logo"
+                            width={190}
+                            height={100}
+                        />
+                    </Link>
+                    {/* ? DEV Only */}
+                    {devEnv && <DevDebug />}
+                    <LanguageSelector />
+                    <ThemeSwitcher />
+                </Card>
+            </nav>
+        </header>
     );
 };
 
