0a7da203ab08c17b1d3e7161b4e50c0622f57dfe
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-06 07:15:02 +0400

    feat: New seo ts file

---
+++ lib/seo.ts
@@ -0,0 +1,8 @@
+export const ROOTKEYWORDS = [
+    "invoice",
+    "invoice generator",
+    "invoice generating",
+    "invoice app",
+    "invoice generator app",
+    "free invoice generator",
+];
