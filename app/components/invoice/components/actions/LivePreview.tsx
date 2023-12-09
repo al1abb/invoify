18bacdc78d2b29ff41980413c3d87fe582c3fc21
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-09 19:42:42 +0400

    refactor: Add border to dynamic invoice on dark mode

--- app/components/invoice/components/actions/LivePreview.tsx
+++ app/components/invoice/components/actions/LivePreview.tsx
@@ -12,7 +12,7 @@ export default function LivePreview({ data }: LivePreviewProps) {
     return (
         <>
             <Subheading>Live Preview:</Subheading>
-            <div className="border dark:border-none rounded-xl my-1">
+            <div className="border dark:border-gray-600 rounded-xl my-1">
                 <DynamicInvoiceTemplate {...data} />
             </div>
         </>
