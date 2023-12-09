244bfcf8d4a2b2e104d6f782630d9465da7f5745
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-09 10:36:48 +0400

    chore: Removed unused i18n server side approach

--- app/i18n/i18n.ts
+++
@@ -1,8 +0,0 @@
-// ? NOTE:
-// Below code is used for server component i18n approach logic
-
-// import { getRequestConfig } from "next-intl/server";
-
-// export default getRequestConfig(async ({ locale }) => ({
-//     messages: (await import(`./locales/${locale}.json`)).default,
-// }));
