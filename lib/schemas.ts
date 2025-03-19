9fe37bd95d381c5cf6a20395a238423d5c0e1a5b
TiagoAzevedo03 <tiagoazevedo03@gmail.com>
2025-03-19 16:43:51 +0000

    handle input larger that js limits

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -54,7 +54,8 @@ const fieldValidators = {
         .min(1, { message: "Must be a number greater than 0" }),
     unitPrice: z.coerce
         .number()
-        .gt(0, { message: "Must be a number greater than 0" }),
+        .gt(0, { message: "Must be a number greater than 0" })
+        .lte(Number.MAX_SAFE_INTEGER, { message: `Must be ≤ ${Number.MAX_SAFE_INTEGER}` }),
 
     // Strings
     string: z.string(),
