733fae3e06880820e9b9818f4418ca3c346a67bb
wsalas19 <willbullet52@gmail.com>
2025-04-03 15:25:36 -0500

    fix: Firefox pdf generation solved and better theme switcher button.

--- app/components/reusables/ThemeSwitcher.tsx
+++ app/components/reusables/ThemeSwitcher.tsx
@@ -9,19 +9,20 @@ import { BaseButton } from "@/app/components";
 import { Moon, Sun } from "lucide-react";
 
 const ThemeSwitcher = () => {
-    const { theme, setTheme } = useTheme();
-    return (
-        <>
-            <BaseButton
-                size="icon"
-                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-            >
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
-            </BaseButton>
-        </>
-    );
+	const { theme, setTheme } = useTheme();
+	return (
+		<>
+			<BaseButton
+				variant='ghost'
+				size='icon'
+				onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+			>
+				<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+				<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+				<span className='sr-only'>Toggle theme</span>
+			</BaseButton>
+		</>
+	);
 };
 
 export default ThemeSwitcher;
