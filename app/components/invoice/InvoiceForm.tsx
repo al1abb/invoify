3cc236d2bab04bd4ca5d3bdb9747bc63daa46c47
Raif Abdul Razak <rarofcl@gmail.com>
2025-05-04 10:52:23 +0400

    chore: remove bill from section

--- app/components/invoice/InvoiceForm.tsx
+++ app/components/invoice/InvoiceForm.tsx
@@ -7,11 +7,11 @@ import { useFormContext, useWatch } from "react-hook-form";
 
 // ShadCn
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 
@@ -20,88 +20,82 @@ import { Wizard } from "react-use-wizard";
 
 // Components
 import {
-    WizardStep,
-    BillFromSection,
-    BillToSection,
-    InvoiceDetails,
-    Items,
-    PaymentInformation,
-    InvoiceSummary,
+  WizardStep,
+  BillFromSection,
+  BillToSection,
+  InvoiceDetails,
+  Items,
+  PaymentInformation,
+  InvoiceSummary,
 } from "@/app/components";
 
 // Contexts
 import { useTranslationContext } from "@/contexts/TranslationContext";
 
 const InvoiceForm = () => {
-    const { _t } = useTranslationContext();
+  const { _t } = useTranslationContext();
 
-    const { control } = useFormContext();
+  const { control } = useFormContext();
 
-    // Get invoice number variable
-    const invoiceNumber = useWatch({
-        name: "details.invoiceNumber",
-        control,
-    });
+  // Get invoice number variable
+  const invoiceNumber = useWatch({
+    name: "details.invoiceNumber",
+    control,
+  });
 
-    const invoiceNumberLabel = useMemo(() => {
-        if (invoiceNumber) {
-            return `#${invoiceNumber}`;
-        } else {
-            return _t("form.newInvBadge");
-        }
-    }, [invoiceNumber]);
+  const invoiceNumberLabel = useMemo(() => {
+    if (invoiceNumber) {
+      return `#${invoiceNumber}`;
+    } else {
+      return _t("form.newInvBadge");
+    }
+  }, [invoiceNumber]);
 
-    return (
-        <div className={`xl:w-[55%]`}>
-            <Card>
-                <CardHeader>
-                    <div className="flex gap-3">
-                        <CardTitle className="flex items-center gap-3">
-                            <span className="uppercase">
-                                {_t("form.title")}
-                            </span>
-                        </CardTitle>
-                        <Badge variant="secondary" className="w-fit">
-                            <p style={{ fontSize: "14px" }}>
-                                {invoiceNumberLabel}
-                            </p>
-                        </Badge>
-                    </div>
-                    <CardDescription>{_t("form.description")}</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <div className="space-y-8">
-                        <Wizard>
-                            <WizardStep>
-                                <div className="flex gap-x-20 gap-y-10">
-                                    <BillFromSection />
+  return (
+    <div className={`xl:w-[55%]`}>
+      <Card>
+        <CardHeader>
+          <div className="flex gap-3">
+            <CardTitle className="flex items-center gap-3">
+              <span className="uppercase">{_t("form.title")}</span>
+            </CardTitle>
+            <Badge variant="secondary" className="w-fit">
+              <p style={{ fontSize: "14px" }}>{invoiceNumberLabel}</p>
+            </Badge>
+          </div>
+          <CardDescription>{_t("form.description")}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-8">
+            <Wizard>
+              <WizardStep>
+                <div className="flex gap-x-20 gap-y-10">
+                  <BillToSection />
+                </div>
+              </WizardStep>
+              <WizardStep>
+                <div className="flex flex-wrap gap-y-10">
+                  <InvoiceDetails />
+                </div>
+              </WizardStep>
 
-                                    <BillToSection />
-                                </div>
-                            </WizardStep>
-                            <WizardStep>
-                                <div className="flex flex-wrap gap-y-10">
-                                    <InvoiceDetails />
-                                </div>
-                            </WizardStep>
+              <WizardStep>
+                <Items />
+              </WizardStep>
 
-                            <WizardStep>
-                                <Items />
-                            </WizardStep>
+              <WizardStep>
+                <PaymentInformation />
+              </WizardStep>
 
-                            <WizardStep>
-                                <PaymentInformation />
-                            </WizardStep>
-
-                            <WizardStep>
-                                <InvoiceSummary />
-                            </WizardStep>
-                        </Wizard>
-                    </div>
-                </CardContent>
-            </Card>
-        </div>
-    );
+              <WizardStep>
+                <InvoiceSummary />
+              </WizardStep>
+            </Wizard>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default InvoiceForm;
--- app/components/invoice/form/wizard/WizardProgress.tsx
+++ app/components/invoice/form/wizard/WizardProgress.tsx
@@ -16,115 +16,115 @@ import { useTranslationContext } from "@/contexts/TranslationContext";
 import { InvoiceType, WizardStepType } from "@/types";
 
 type WizardProgressProps = {
-    wizard: WizardValues;
+  wizard: WizardValues;
 };
 
 const WizardProgress = ({ wizard }: WizardProgressProps) => {
-    const { activeStep, stepCount } = wizard;
+  const { activeStep, stepCount } = wizard;
 
-    const {
-        formState: { errors },
-    } = useFormContext<InvoiceType>();
+  const {
+    formState: { errors },
+  } = useFormContext<InvoiceType>();
 
-    const { _t } = useTranslationContext();
+  const { _t } = useTranslationContext();
 
-    const step1Valid = !errors.sender && !errors.receiver;
-    const step2Valid =
-        !errors.details?.invoiceNumber &&
-        !errors.details?.dueDate &&
-        !errors.details?.invoiceDate &&
-        !errors.details?.currency;
+  const step1Valid = !errors.sender && !errors.receiver;
+  const step2Valid =
+    !errors.details?.invoiceNumber &&
+    !errors.details?.dueDate &&
+    !errors.details?.invoiceDate &&
+    !errors.details?.currency;
 
-    const step3Valid = !errors.details?.items;
-    const step4Valid = !errors.details?.paymentInformation;
-    const step5Valid =
-        !errors.details?.paymentTerms &&
-        !errors.details?.subTotal &&
-        !errors.details?.totalAmount &&
-        !errors.details?.discountDetails?.amount &&
-        !errors.details?.taxDetails?.amount &&
-        !errors.details?.shippingDetails?.cost;
+  const step3Valid = !errors.details?.items;
+  const step4Valid = !errors.details?.paymentInformation;
+  const step5Valid =
+    !errors.details?.paymentTerms &&
+    !errors.details?.subTotal &&
+    !errors.details?.totalAmount &&
+    !errors.details?.discountDetails?.amount &&
+    !errors.details?.taxDetails?.amount &&
+    !errors.details?.shippingDetails?.cost;
 
-    /**
-     * Determines the button variant based on the given WizardStepType.
-     *
-     * @param {WizardStepType} step - The wizard step object
-     * @returns The button variant ("destructive", "default", or "outline") based on the step's validity and active status.
-     */
-    const returnButtonVariant = (step: WizardStepType) => {
-        if (!step.isValid) {
-            return "destructive";
-        }
-        if (step.id === activeStep) {
-            return "default";
-        } else {
-            return "outline";
-        }
-    };
+  /**
+   * Determines the button variant based on the given WizardStepType.
+   *
+   * @param {WizardStepType} step - The wizard step object
+   * @returns The button variant ("destructive", "default", or "outline") based on the step's validity and active status.
+   */
+  const returnButtonVariant = (step: WizardStepType) => {
+    if (!step.isValid) {
+      return "destructive";
+    }
+    if (step.id === activeStep) {
+      return "default";
+    } else {
+      return "outline";
+    }
+  };
 
-    /**
-     * Checks whether the given WizardStepType has been passed or not.
-     *
-     * @param {WizardStepType} currentStep - The WizardStepType object
-     * @returns `true` if the step has been passed, `false` if it hasn't, or `undefined` if the step is not valid.
-     */
-    const stepPassed = (currentStep: WizardStepType) => {
-        if (currentStep.isValid) {
-            return activeStep > currentStep.id ? true : false;
-        }
-    };
+  /**
+   * Checks whether the given WizardStepType has been passed or not.
+   *
+   * @param {WizardStepType} currentStep - The WizardStepType object
+   * @returns `true` if the step has been passed, `false` if it hasn't, or `undefined` if the step is not valid.
+   */
+  const stepPassed = (currentStep: WizardStepType) => {
+    if (currentStep.isValid) {
+      return activeStep > currentStep.id ? true : false;
+    }
+  };
 
-    const steps: WizardStepType[] = [
-        {
-            id: 0,
-            label: _t("form.wizard.fromAndTo"),
-            isValid: step1Valid,
-        },
-        {
-            id: 1,
-            label: _t("form.wizard.invoiceDetails"),
-            isValid: step2Valid,
-        },
-        {
-            id: 2,
-            label: _t("form.wizard.lineItems"),
-            isValid: step3Valid,
-        },
-        {
-            id: 3,
-            label: _t("form.wizard.paymentInfo"),
-            isValid: step4Valid,
-        },
-        {
-            id: 4,
-            label: _t("form.wizard.summary"),
-            isValid: step5Valid,
-        },
-    ];
+  const steps: WizardStepType[] = [
+    {
+      id: 0,
+      label: _t("form.wizard.to"),
+      isValid: step1Valid,
+    },
+    {
+      id: 1,
+      label: _t("form.wizard.invoiceDetails"),
+      isValid: step2Valid,
+    },
+    {
+      id: 2,
+      label: _t("form.wizard.lineItems"),
+      isValid: step3Valid,
+    },
+    {
+      id: 3,
+      label: _t("form.wizard.paymentInfo"),
+      isValid: step4Valid,
+    },
+    {
+      id: 4,
+      label: _t("form.wizard.summary"),
+      isValid: step5Valid,
+    },
+  ];
 
-    return (
-        <div className="flex flex-wrap justify-around items-center gap-y-3">
-            {steps.map((step, idx) => (
-                <div key={step.id} className="flex items-center">
-                    <BaseButton
-                        variant={returnButtonVariant(step)}
-                        className="w-auto"
-                        onClick={() => {
-                            wizard.goToStep(step.id);
-                        }}
-                    >
-                        {step.id + 1}. {step.label}
-                    </BaseButton>
+  return (
+    <div className="flex flex-wrap justify-around items-center gap-y-3">
+      {steps.map((step, idx) => (
+        <div key={step.id} className="flex items-center">
+          <BaseButton
+            variant={returnButtonVariant(step)}
+            className="w-auto"
+            onClick={() => {
+              wizard.goToStep(step.id);
+            }}
+          >
+            {step.id + 1}. {step.label}
+          </BaseButton>
 
-                    {/* {step.id != stepCount - 1 && (
+          {/* {step.id != stepCount - 1 && (
                         <div>
                             <Dot />
                         </div>
                     )} */}
-                </div>
-            ))}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default WizardProgress;
