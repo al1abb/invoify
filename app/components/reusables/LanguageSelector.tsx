c7bdca97faf72eb6c58e56d5d54d7c9bc4038de0
Raif Abdul Razak <rarofcl@gmail.com>
2025-05-06 13:25:48 +0400

    chore: revert to old next and old next-intl

--- app/components/reusables/LanguageSelector.tsx
+++ app/components/reusables/LanguageSelector.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { useParams } from "next/navigation";
+
 // Next Intl
-import { usePathname, useRouter } from "@/i18n/routing";
-import { useLocale } from "next-intl";
+import { useRouter } from "next-intl/client"; // This useRouter is wrapped with next/navigation useRouter
 
 // ShadCn
 import {
@@ -14,23 +15,23 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Badge } from "@/components/ui/badge";
 
 // Variables
 import { LOCALES } from "@/lib/variables";
 
 const LanguageSelector = () => {
-  const locale = useLocale();
   const router = useRouter();
-  const pathname = usePathname();
+  const params = useParams();
 
   const handleLanguageChange = (lang: string) => {
     console.log(lang);
+
     router.push("/", { locale: lang });
   };
-
   return (
     <Select
-      value={pathname}
+      value={params.locale.toLocaleString()}
       onValueChange={(lang) => handleLanguageChange(lang)}
     >
       <SelectTrigger className="w-[10rem] relative" aria-label="Languages">
