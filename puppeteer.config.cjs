35897cb6fe7c212c40ff14fedee35716b3a7b654
Raif Abdul Razak <rarofcl@gmail.com>
2025-05-06 21:56:24 +0400

    chore: update chrome path

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -4,6 +4,8 @@ const { join } = require("path");
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-    // Changes the cache location for Puppeteer.
-    cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: process.env.VERCEL
+    ? "/vercel/path0/.cache/puppeteer"
+    : join(__dirname, ".cache", "puppeteer"),
 };
