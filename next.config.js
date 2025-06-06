9cfb3550f8a872cd1c18488cad1d9657f76a4d4d
Karl Horky <karl.horky@gmail.com>
2025-06-06 12:40:47 +0200

    Fix Next.js config, add .editorconfig with 4 spaces indentation config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ["puppeteer-core"],
-    },
+    serverExternalPackages: ["puppeteer-core"],
     webpack: (config) => {
         config.module.rules.push({
             test: /\.map$/,
