cf35239fdae21d48137ebfa16e45a8ec26479390
Karl Horky <karl.horky@gmail.com>
2025-09-02 13:15:56 +0200

    Revert comment wrapping changes

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,7 @@ import { routing } from "@/i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-    // Skip all paths that should not be internationalized. This
-    // example skips certain folders and all pathnames with a dot
-    // (e.g. favicon.ico)
+    // Skip all paths that should not be internationalized. This example skips
+    // certain folders and all pathnames with a dot (e.g. favicon.ico)
     matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
