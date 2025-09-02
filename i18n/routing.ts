4bff9bd1afc572c09391ede5fa09c1e33e71998d
Karl Horky <karl.horky@gmail.com>
2025-09-02 13:13:43 +0200

    Simplify

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,10 +2,6 @@ import { defineRouting } from "next-intl/routing";
 import { DEFAULT_LOCALE, LOCALES } from "@/lib/variables";
 
 export const routing = defineRouting({
-    locales: LOCALES.map((l) => l.code) as unknown as readonly string[],
+    locales: LOCALES.map(({ code }) => code),
     defaultLocale: DEFAULT_LOCALE,
-    // Keep default locale prefix behavior (v3 switched default to 'always')
-    localePrefix: "always",
 });
-
-export type AppLocale = (typeof routing.locales)[number];
